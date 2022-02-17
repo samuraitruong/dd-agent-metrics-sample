60d50de33d3568c493dc3a046b9d5e208f6b0d1f
Truong Nguyen <samuraitruong@hotmail.com>
2022-02-17 23:42:37 +1100

    chore: update readme

--- index.js
+++ index.js
@@ -16,13 +16,14 @@ import StatsD from 'hot-shots';
     const tags = ['app:test', 'version:v1'];
     const rvalue = Math.random() * 10;
 
-    client.increment('test.app.hello', 1, 1, tags);
+    client.increment('test.app.request', 1, 1, tags);
+    client.increment('test.app.count', Math.ceil(Math.random() * 10), 1, tags);
     // client.count('test.app.count', 1, 1, tags);
     client.distribution('test.app.latency', rvalue, 1, tags);
     client.gauge('test.app.gauge', rvalue, 1, tags);
     client.histogram('test.app.histogram', rvalue, 1, tags);
 
-    res.json({ ok: 1 });
+    res.json({ ts: new Date().toISOString() });
   });
   app.listen(3000, () => {
     console.log('App running at localhost:3000');
--- readme.md
+++ readme.md
@@ -16,4 +16,10 @@ docker-compose up
 
 ## View metrics
 
-https://app.datadoghq.com/metric/explorer?exp_metric=test.app.hello%2Ctest.app.started%2Ctest.app.latency%2Ctest.app.gauge%2Ctest.app.histogram.count&from_ts=1645100910004&to_ts=1645101210004&live=true&tile_size=m&exp_agg=sum&exp_row_type=metric
+https://app.datadoghq.com/metric/explorer?exp_metric=test.app.started%2Ctest.app.latency%2Ctest.app.gauge%2Ctest.app.histogram.count%2Ctest.app.count%2Ctest.app.request&from_ts=1645101377273&to_ts=1645101677273&live=true&tile_size=m&exp_agg=sum&exp_row_type=metric
+
+## Trigger
+
+```
+watch -n1 -x curl http://localhost:3000
+```
